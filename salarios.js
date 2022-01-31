7f152f62545c64326c0779a03e335f7c054801e2
Juan Camilo <juanktronica@hotmail.com>
2022-01-31 09:31:56 -0500

    Se ha terminado la practica utilizando JavaScript

---
+++ salarios.js
@@ -0,0 +1,101 @@
+const colombia = [];
+
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "Natalia",
+    salary: 500,
+});
+
+colombia.push({
+    name: "Dario",
+    salary: 800,
+});
+
+colombia.push({
+    name: "Hector",
+    salary: 2000,
+});
+
+colombia.push({
+    name: "Camila",
+    salary: 440,
+});
+
+colombia.push({
+    name: "Santiago",
+    salary: 1450,
+});
+
+colombia.push({
+    name: "Ivan",
+    salary: 600,
+});
+
+colombia.push({
+    name: "Martha",
+    salary: 900,
+});
+
+colombia.push({
+    name: "Daniela",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "Daniel",
+    salary: 500,
+});
+
+colombia.push({
+    name: "Camilo",
+    salary: 800,
+});
+
+colombia.push({
+    name: "Tatiana",
+    salary: 2000,
+});
+
+colombia.push({
+    name: "Luz",
+    salary: 440,
+});
+
+colombia.push({
+    name: "Marlos",
+    salary: 1450,
+});
+
+colombia.push({
+    name: "Julio",
+    salary: 600,
+});
+
+colombia.push({
+    name: "Augusto",
+    salary: 900,
+});
+
+colombia.push({
+    name: "Cesar",
+    salary: 440,
+});
+
+colombia.push({
+    name: "Carlos",
+    salary: 1450,
+});
+
+colombia.push({
+    name: "Juliana",
+    salary: 600,
+});
+
+colombia.push({
+    name: "Nora",
+    salary: 900,
+});
